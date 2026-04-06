a7842287a3d3dfd20e6f1b32a1c04cefd8da936d
yejoo0310 <yejoo03105@gmail.com>
2026-04-06 14:12:21 +0900

    feat: Quiz 클래스 기본 메서드(init, display, is_correct, to_dict) 구현

---
+++ main.py
@@ -0,0 +1,15 @@
+from models import Quiz
+
+def main():
+    q1 = Quiz("1 + 1은?", ["1", "2", "3", "4"], 2)
+
+    q1.display(1)
+
+    user_ans = int(input("정답 입력: "))
+    if (q1.is_correct(user_ans)):
+        print("ans")
+    else:
+        print("no")
+
+if __name__ == "__main__":
+    main()
